5ed5dd972f1fc2b5aa93f81c7d5ef394bcc0d43e
tamerlan053 <97560103+tamerlan053@users.noreply.github.com>
2024-04-16 23:17:11 +0200

    Update MainWindow.xaml.cs
    final version

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,9 +8,25 @@ namespace HandwashTimer
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeTimer();
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromHours(1);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Wash your hands", "reminder!", MessageBoxButton.OK, MessageBoxImage.Information);
         }
     }
 }
